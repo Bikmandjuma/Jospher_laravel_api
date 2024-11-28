f0e0dabf0f8cd76061cdf547a6045612d62c615b
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2024-11-28 10:36:45 +0200

    test db

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\customer;
+use Illuminate\Support\Facades\DB;
 
 class ManagerController extends Controller
 {
