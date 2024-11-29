4953c16c31d5437f3ae8d88c8c9780e85bfa0399
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2024-11-29 15:23:04 +0200

    still working on google auth

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -21,40 +21,20 @@ class SocialiteController extends Controller
     public function login_by_google_callback(Request $request)
     {
         try {
-            // Get the user data from Google
+
             $user = Socialite::driver('google')->stateless()->user();
 
-            // Check if the user already exists in the database
-            $existingUser = User::where('provider_id', $user->id)->first();
-
-            if ($existingUser) {
-
-                return redirect('https://jobsphererdaflask-production.up.railway.app/user/dashboard');
-                
-            } else {
-
-                $profilePicture = $user->avatar ?? 'default_image_url';
-                $birthdate = $user->user['birthday'] ?? null;
-                $phone = $user->phone ?? 'N/A';
-                $gender = $user->user['gender'] ?? null;
-
-                // Create a new user in the database
-                $newUser = User::create([
+            User::create([
                     'names' => $user->name,
                     'provider_name' => 'Google',
                     'provider_id' => $user->id,
                     'email' => $user->email,
-                    'phone' => $phone,
-                    'image' => $profilePicture,
-                    'dob' => $birthdate,
-                    'gender' => $gender,
-                ]);
+                   
+            ]);
 
-                return redirect('https://jobsphererdaflask-production.up.railway.app/user/dashboard');
-            }
+            return redirect('https://jobsphererdaflask-production.up.railway.app/user/dashboard');
 
         } catch (Exception $e) {
-            // Return a response indicating something went wrong
             return response()->json(['error' => 'Something went wrong: ' . $e->getMessage()], 500);
         }
 
