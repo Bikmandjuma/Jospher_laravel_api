095aac71645191a0e6b7df44c1818f035977ae31
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2024-11-29 14:59:22 +0200

    still working on google auth

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -14,58 +14,14 @@ class SocialiteController extends Controller
 {
     public function login_by_google()
     {
-        return Socialite::driver('google')->stateless()->scopes(['email', 'profile', 'openid'])->redirect();
+        // Initiating Google login process
+        return Socialite::driver('google')->redirect();
     }
 
     public function login_by_google_callback(Request $request)
     {
-        try {
-
-            $user = Socialite::driver('google')->stateless()->user();
-
-            Log::info('Google user data:', $user->toArray());
-
-            $existingUser = User::where('provider_id', $user->id)->first();
-
-            if ($existingUser) {
-                $token = Auth::guard('user')->login($existingUser);
-
-                // return redirect('https://jobsphererdaflask-production.up.railway.app/user/dashboard?token=' . $token);
-                return redirect()->json([
-                    'massage' => "Existing user's google data login !"
-                ]);
-
-            }else{
-
-                $profilePicture = $user->avatar;
-                $birthdate = isset($user->user['birthday']) ? $user->user['birthday'] : null;
-                $phone =$user->phone ?? 'N/A';
-                $image = $user->avatar ?? 'default_image_url';
-
-                $newUser = User::create([
-                    'names' => $user->name,
-                    'provider_name' => 'Google',
-                    'provider_id' => $user->id,
-                    'email' => $user->email,
-                    'phone' => $phone,
-                    'image' => $image,
-                    'dob' => $birthdate,
-                    'gender' => $user->gender,
-                ]);
-
-                $token = Auth::guard('user')->login($newUser);
-
-                // return redirect('https://jobsphererdaflask-production.up.railway.app/user/dashboard?token=' . $token);
-                return redirect()->json([
-                    'massage' => "new user's google data added !"
-                ]);
-            }
-
-        } catch (Exception $e) {
-            Log::error('Google login error: ' . $e->getMessage());
-            return response()->json(['error' => 'Something went wrong: ' . $e->getMessage()], 500);
-        }
-
+        return response()->json([
+            'message' => "New user's Google",
+        ]);
     }
-
 }
