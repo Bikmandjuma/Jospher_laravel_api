8f4d5c69d0ffe9886947c8c23c1f3ffc3d393816
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2024-11-30 13:11:30 +0200

    new stuff added

--- app/Http/Controllers/SocialLoginController.php
+++ app/Http/Controllers/SocialLoginController.php
@@ -40,10 +40,10 @@ class SocialLoginController extends Controller
                     ]
                 );
 
-                User::create([
-                    'name' =>  $socialUser->name,
-                    'email' =>  $socialUser->email,
-                ]);
+                // User::create([
+                //     'name' =>  $socialUser->name,
+                //     'email' =>  $socialUser->email,
+                // ]);
                 
                 return redirect('https://jobsphererdaflask-production.up.railway.app/seeker/dashboard');
 
