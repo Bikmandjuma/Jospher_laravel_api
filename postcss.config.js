086cb20a26dfb9c0170e888ee0ecfc71b5fdf6eb
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-02-19 01:54:05 +0200

    postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-// export default {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// }
-
 module.exports = {
   plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-  ]
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
-
