f8368ff579716e544444f11611897a3f48dda6e4
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-02-19 04:44:09 +0200

    new stuff added

--- app/Http/Middleware/UpdateLastActive.php
+++ app/Http/Middleware/UpdateLastActive.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class UpdateLastActive
 {
@@ -13,8 +15,17 @@ class UpdateLastActive
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next , $guard = null): Response
     {
+        if (Auth::guard('user')->check()) {
+            $user = Auth::guard('user')->user();
+            $user->update(['last_active_at' => now()]);
+
+            // Log to check if middleware is running
+            Log::info('User last_active_at updated', ['user_id' => $user->id, 'time' => now()]);
+        } else {
+            Log::warning('Middleware executed but user not authenticated');
+        }
         return $next($request);
     }
 }
