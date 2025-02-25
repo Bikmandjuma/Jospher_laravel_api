cc60844b39a0958391cb68f237a7896e689bc362
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-02-25 15:17:39 +0200

    new models added

--- app/Models/admin.php
+++ app/Models/admin.php
@@ -11,11 +11,8 @@ class Admin extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    // Define the table associated with the model (if not following Laravel's convention)
-    protected $table = 'admins';
-    protected $guard = 'admin';
+    protected $table = 'admins'; // Define table if different from Laravel's default
 
-    // Define which attributes are mass assignable
     protected $fillable = [
         'firstname',
         'lastname',
@@ -28,23 +25,13 @@ class Admin extends Authenticatable
         'password',
     ];
 
-    // Protect against mass-assignment vulnerability
-    protected $guarded = [];
-
-    // Attributes that should be hidden when the model is converted to an array or JSON
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    // Casts certain attributes to native types
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'dob' => 'date', // Assuming 'dob' is a date column in the database
+        'dob' => 'date',
     ];
-
-    // Optionally, define timestamps if not using the default 'created_at' and 'updated_at'
-    // public $timestamps = false;
-
-    // If you want to make any additional custom methods or attributes, you can do that here.
 }
--- config/auth.php
+++ config/auth.php
@@ -10,7 +10,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'admins',
         ],
 
         'admin' => [
